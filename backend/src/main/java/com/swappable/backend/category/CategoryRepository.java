46d4dabcc6f9fbc15c662ba5f88a538315179ae7
Joe Quigley <joeyjtq@hotmail.com>
2026-06-01 15:16:10 +0100

    Add Item Backend and link to Item Detail

--- backend/src/main/java/com/swappable/backend/category/CategoryRepository.java
+++ backend/src/main/java/com/swappable/backend/category/CategoryRepository.java
@@ -2,5 +2,5 @@ package com.swappable.backend.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
