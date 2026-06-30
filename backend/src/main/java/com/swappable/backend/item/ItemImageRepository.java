8f7ebe6e2a89776147c1dd4ece1f94651bae94e6
rolivasarhelyi <roli.vasarhelyi@gmail.com>
2026-06-30 11:36:01 +0200

    Remove unused ItemImage repository query method

--- backend/src/main/java/com/swappable/backend/item/ItemImageRepository.java
+++ backend/src/main/java/com/swappable/backend/item/ItemImageRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ItemImageRepository extends JpaRepository<ItemImage, Integer> {
-    List<ItemImage> findByItemIdOrderByDisplayOrderAsc(Integer itemId);
     long countByItemId(Integer itemId);
 
     @Query("select img.id from ItemImage img where img.item.id = :itemId order by img.displayOrder asc")
