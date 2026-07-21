c660ef15daa23dfe13a00eb438c73667606baeea
hazel <h.cunningham8@universityofgalway.ie>
2026-07-21 15:11:55 +0100

    Fix missing ResponseEntity import in controller

--- backend/src/main/java/com/swappable/backend/swaprequest/SwapRequestController.java
+++ backend/src/main/java/com/swappable/backend/swaprequest/SwapRequestController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
